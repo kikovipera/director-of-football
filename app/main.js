15f60570b80e0dad2e3453b17c7bffb1867f0af7
Brad Davies <bradov@null.net>
2016-09-17 22:42:34 +0100

    Updated project structure. App recoding begins.

---
+++ app/main.js
@@ -0,0 +1,5 @@
+const electron = require('electron')
+
+const app = electron.app
+
+app.quit()
--- src/pages/js/splash.js
+++
@@ -1,6 +0,0 @@
-const $ = require('jquery')
-const ipc = require('electron').ipcRenderer
-
-$('#text-button').on('click', (e) => {
-  ipc.send('console-log', $('#text-field').val())
-})
