44bb2f6728f50c2b103b740880692ddfed3c4ffc
Brad Davies <bradov@null.net>
2016-09-17 23:41:50 +0100

    Initial window setup

--- app/main.js
+++ app/main.js
@@ -1,5 +1,34 @@
+const argv = require('yargs').argv
 const electron = require('electron')
+const path = require('path')
+
+const BrowserWindow = electron.BrowserWindow
 
 const app = electron.app
 
-app.quit()
+let initialWindow
+
+function ready () {
+  // Initial log messages
+  console.log(app.getName() + ' v' + app.getVersion() + '\nElectron v' + process.versions.electron)
+  if (argv.debug) console.log('DEBUG MODE')
+  // Create initialWindow
+  createInitialWindow()
+}
+
+function createInitialWindow () {
+  initialWindow = new BrowserWindow({width: 600, height: 500, title: app.getName(), show: false})
+
+  // Window events
+  initialWindow.on('ready-to-show', () => {
+    initialWindow.show()
+  })
+  initialWindow.on('closed', () => {
+    initialWindow = null
+  })
+
+  // Load URL
+  initialWindow.loadURL(path.join('file://', __dirname, '/windows/initial/index.html'))
+}
+
+app.on('ready', ready)
